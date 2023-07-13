543f63e1d29eb1b5fe0fb4328963cb63a8cb1e37
Lorenzo Ruozzi <lollor98@hotmail.it>
2023-07-13 11:43:07 +0200

    Remove unnecessary annotation

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -82,7 +82,6 @@ class SearchController extends AbstractController
      */
     public function postAction(Request $request)
     {
-        /** @var array $inputBag */
         $inputBag = $request->request->all('monsieurbiz_searchplugin_search');
         /** @var ?string $query */
         $query = $inputBag['query'] ?? null;
